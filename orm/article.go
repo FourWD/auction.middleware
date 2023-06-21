7e3c7d56d9f821e76093d6a35ab645e07fd43309
puarns <puarns@gmail.com>
2023-06-21 23:12:46 +0700

    no message

--- orm/article.go
+++ orm/article.go
@@ -8,8 +8,8 @@ type Article struct {
 
 	ArticleTypeID string `json:"article_type_id" query:"article_type_id" db:"article_type_id" gorm:"type:varchar(36)"`
 	FileCoverID   string `json:"file_cover_id" query:"file_cover_id" db:"file_cover_id" gorm:"type:varchar(36)"`
-	Subject       string `json:"subject" query:"subject" db:"subject" gorm:"type:varchar(100)"`
-	Detail        string `json:"detail" query:"detail" db:"detail" gorm:"type:varchar(100)"`
+	Subject       string `json:"subject" query:"subject" db:"subject" gorm:"type:varchar(500)"`
+	Detail        string `json:"detail" query:"detail" db:"detail" gorm:"type:varchar(500)"`
 	ViewCount     int    `json:"view_count" query:"view_count" db:"view_count" gorm:"type:int(11)"`
 	orm.RowOrder
 }
--- orm/banner.go
+++ orm/banner.go
@@ -8,9 +8,10 @@ type Banner struct {
 
 	BannerGroupID string `json:"banner_grounp_id" query:"banner_grounp_id" db:"banner_grounp_id" gorm:"type:varchar(36)"`
 
-	ImagePath     string `json:"image_path" query:"image_path" db:"image_path" gorm:"type:varchar(100)"`
-	Url           string `json:"url" query:"url" db:"url" gorm:"type:varchar(100)"`
-	Youtube       string `json:"youtube" query:"youtube" db:"youtube" gorm:"type:varchar(100)"`
-	Description   string `json:"description" query:"description" db:"description" gorm:"type:varchar(100)"`
+	Subject     string `json:"subject" query:"subject" db:"subject" gorm:"type:varchar(500)"`
+	ImagePath   string `json:"image_path" query:"image_path" db:"image_path" gorm:"type:varchar(100)"`
+	Url         string `json:"url" query:"url" db:"url" gorm:"type:varchar(100)"`
+	Youtube     string `json:"youtube" query:"youtube" db:"youtube" gorm:"type:varchar(100)"`
+	Description string `json:"description" query:"description" db:"description" gorm:"type:varchar(500)"`
 	orm.RowOrder
 }
