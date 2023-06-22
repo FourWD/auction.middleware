6d422aa18d36b98fca6dfc0e2ea5ae59f850831c
puarns <puarns@gmail.com>
2023-06-22 15:18:57 +0700

    no message

--- orm/auction-vehicle.go
+++ orm/auction-vehicle.go
@@ -6,7 +6,7 @@ type AuctionVehicle struct {
 	ID string `json:"id" query:"id" db:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	AuctionID string `json:"finance_id" query:"finance_id" db:"finance_id" gorm:"type:varchar(36)"`
-	TableCode string `json:"table_code" query:"table_code" db:"table_code" gorm:"type:varchar(4)"`
-	VehicleID string `json:"vehicle_id" query:"vehicle_id" db:"vehicle_id" gorm:"type:varchar(36)"`
+	AuctionID        string `json:"finance_id" query:"finance_id" db:"finance_id" gorm:"type:varchar(36)"`
+	VehicleTableCode string `json:"vehicle_table_code" query:"vehicle_table_code" db:"vehicle_table_code" gorm:"type:varchar(4)"`
+	VehicleID        string `json:"vehicle_id" query:"vehicle_id" db:"vehicle_id" gorm:"type:varchar(36)"`
 }
