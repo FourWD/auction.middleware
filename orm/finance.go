9b65382fa5b7e872710f6b0230b194c7889b9ece
Pattanapong <pattanapong.amu@gmail.com>
2023-07-27 17:23:17 +0700

    update finance loan url

--- orm/finance.go
+++ orm/finance.go
@@ -6,9 +6,10 @@ type Finance struct {
 	ID string `json:"id" query:"id" db:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Logo  string `json:"logo" query:"logo" db:"logo" gorm:"type:varchar(50)"`
-	Code  string `json:"code" query:"code" db:"code" gorm:"type:varchar(4)"`
-	Name  string `json:"name" query:"name" db:"name" gorm:"type:varchar(50)"`
-	Color string `json:"color" query:"color" db:"color" gorm:"type:varchar(7)"`
+	Logo    string `json:"logo" query:"logo" db:"logo" gorm:"type:varchar(50)"`
+	Code    string `json:"code" query:"code" db:"code" gorm:"type:varchar(4)"`
+	Name    string `json:"name" query:"name" db:"name" gorm:"type:varchar(50)"`
+	Color   string `json:"color" query:"color" db:"color" gorm:"type:varchar(7)"`
+	LoanUrl string `json:"loan_url" query:"loan_url" db:"loan_url" gorm:"type:varchar(200)"`
 	orm.RowOrder
 }
