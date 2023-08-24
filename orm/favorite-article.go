e60aade5921fafe0b9157b05fd2e93aa20734b12
suradevv <opapayao1@hotmail.com>
2023-08-24 14:48:46 +0700

    update

---
+++ orm/favorite-article.go
@@ -0,0 +1,11 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type FavoriteArticle struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	UserID    string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	ArticleID string `json:"article_id" query:"article_id" gorm:"type:varchar(36)"`
+}
---
+++ orm/favorite-vehicle.go
@@ -0,0 +1,13 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type FavoriteVehicle struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	UserID           string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	AuctionID        string `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
+	VehicleID        string `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
+	VehicleTableName string `json:"vehicle_table_name" query:"vehicle_table_name" gorm:"type:varchar(50)"`
+}
