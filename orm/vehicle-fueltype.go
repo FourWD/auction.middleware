aec895c7bb3aac01d835e1ae86f02bb9fa3816c9
Pattanapong <pattanapong.amu@gmail.com>
2023-08-30 18:17:31 +0700

    Update vehicle-fueltype.go
    Change Struct Name

--- orm/vehicle-fueltype.go
+++ orm/vehicle-fueltype.go
@@ -2,7 +2,7 @@ package orm
 
 import "github.com/FourWD/middleware/orm"
 
-type Fueltype struct {
+type VehicleFueltype struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
