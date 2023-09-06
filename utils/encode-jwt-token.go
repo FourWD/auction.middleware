6b6b2d4af9c0e08d1ba1d2be14231f455cb35f6a
puarns <puarns@gmail.com>
2023-09-06 16:16:35 +0700

    no message

--- utils/encode-jwt-token.go
+++ utils/encode-jwt-token.go
@@ -1,5 +1,27 @@
 package utils
 
-func EncodeJwtToken(req string) {
+import (
+	"errors"
+	"fmt"
 
+	"github.com/FourWD/middleware-auction/model"
+	"github.com/FourWD/middleware/common"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetJwtToken(c *fiber.Ctx) (*model.AuctionToken, error) {
+	bearerToken := c.Get("Authorization")
+
+	err := errors.New("error")
+	if err != nil {
+		fmt.Println("GetJwtToken")
+	}
+	auctionToken := new(model.AuctionToken)
+	auctionToken.UserID, err = common.EncodedJwtToken(bearerToken, "user_id")
+
+	if err == nil {
+		return auctionToken, nil
+	} else {
+		return nil, err
+	}
 }
