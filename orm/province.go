98b9c42c73a451a821a2768f08d4965d53e316d1
Pattanapong <pattanapong.amu@gmail.com>
2023-09-13 18:48:58 +0700

    add country
    province , district and sub-district

--- orm/province.go
+++ orm/province.go
@@ -3,9 +3,11 @@ package orm
 import "github.com/FourWD/middleware/orm"
 
 type Province struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	Name string `json:"name" query:"name" gorm:"not null;type:varchar(100)"`
+	Name      string `json:"name" query:"name" gorm:"not null;type:varchar(50)"`
+	NameShort string `json:"name_short" query:"name_short" gorm:"type:varchar(50)"`
+	NameEn    string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
 	orm.RowOrder
 }
---
+++ orm/sub-district.go
@@ -0,0 +1,15 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type SubDistrict struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(6);primary_key;"`
+	orm.GormModel
+
+	DistrictID string `json:"district_id" query:"district_id" gorm:"type:varchar(4)"`
+
+	Name    string `json:"name" query:"name" gorm:"not null;type:varchar(50)"`
+	NameEn  string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
+	ZipCode string `json:"zip_code" query:"zip_code" gorm:"type:varchar(5)"`
+	orm.RowOrder
+}
