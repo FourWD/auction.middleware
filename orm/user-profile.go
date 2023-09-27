53b71aacfac92f130c8b77e7bb57fc45e559d2d7
puarns <puarns@gmail.com>
2023-09-27 10:26:34 +0700

    update user model

---
+++ orm/user-profile.go
@@ -0,0 +1,15 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type UserProfile struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	UserID                string `json:"user_id" query:"user_id" gorm:"type:varchar(36);"`
+	CompanyRegisterTypeID string `json:"company_register_type_id" query:"company_register_type_id" gorm:"type:varchar(2);"`
+	BusinessTypeID        string `json:"business_type_id" query:"business_type_id" gorm:"type:varchar(2);"`
+	CompanyName           string `json:"company_name" query:"company_name" gorm:"type:varchar(50);"`
+	Tax                   string `json:"tax" query:"tax" gorm:"type:varchar(50);"`
+	HQShowroomName        string `json:"hq_show_room_name" query:"hq_show_room_name" gorm:"type:varchar(500);"`
+}
--- orm/user.go
+++ orm/user.go
@@ -1,6 +1,10 @@
 package orm
 
-import "github.com/FourWD/middleware/orm"
+import (
+	"time"
+
+	"github.com/FourWD/middleware/orm"
+)
 
 type User struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
@@ -9,22 +13,25 @@ type User struct {
 	UserTypeID   string `json:"user_type_id" query:"user_type_id" gorm:"type:varchar(36);"`
 	RmEmployeeID string `json:"rm_employee_id" query:"rm_employee_id" gorm:"type:varchar(36);"`
 
-	Username            string `json:"username" query:"username" gorm:"type:varchar(20);"`
-	Password            string `json:"password" query:"password" gorm:"type:varchar(20);"`
-	PrefixID            int    `json:"prefix_id" query:"prefix_id" gorm:"type:int(1);"`
-	Firstname           string `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
-	Lastname            string `json:"lastname" query:"lastname" gorm:"type:varchar(100);"`
-	FileAvatarID        string `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
-	Mobile              string `json:"mobile" query:"mobile" gorm:"type:varchar(20); unique"`
-	Email               string `json:"email" query:"email" gorm:"type:varchar(20);"`
-	Pin                 string `json:"pin" query:"pin" gorm:"type:varchar(255);"`
-	IsNotiBeforeAuction bool   `json:"is_noti_before_auction" query:"is_noti_before_auction" gorm:"type:bool"`
-	IsNotiDuringAuction bool   `json:"is_noti_during_auction" query:"is_noti_during_auction" gorm:"type:bool"`
-	IsNotiAfterAuction  bool   `json:"is_noti_after_auction" query:"is_noti_after_auction" gorm:"type:bool"`
-	IsUseTouchID        string `json:"is_use_touch_id" query:"is_use_touch_id" gorm:"type:int(6);"`
-	CountWrongLogin     int    `json:"count_wrong_login" query:"count_wrong_login" gorm:"type:int(1);"`
-	LastLoginDate       string `json:"last_login_date" query:"last_login_date" gorm:"type:varchar(15)"`
-	UserStatus          string `json:"user_status" query:"user_status" gorm:"type:varchar(15)"`                   // ban approv
-	UserRegisterStatus  string `json:"user_register_status" query:"user_register_status" gorm:"type:varchar(15)"` //สถานะหน้าสมัคร ถึงขันไหนละ 1 otp เสร็จ 2 กรอกข้อมูลเสร็จ 3 แอดมินแอพพรูฟ
-	RightDeposit        int    `json:"right_deposit" query:"right_deposit" gorm:"type:int(5)"`
+	Username            string    `json:"username" query:"username" gorm:"type:varchar(20);"`
+	Password            string    `json:"password" query:"password" gorm:"type:varchar(20);"`
+	PrefixID            int       `json:"prefix_id" query:"prefix_id" gorm:"type:int(1);"`
+	Firstname           string    `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
+	Lastname            string    `json:"lastname" query:"lastname" gorm:"type:varchar(100);"`
+	FileAvatarID        string    `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
+	Mobile              string    `json:"mobile" query:"mobile" gorm:"type:varchar(20); unique"`
+	Email               string    `json:"email" query:"email" gorm:"type:varchar(20);"`
+	Pin                 string    `json:"pin" query:"pin" gorm:"type:varchar(255);"`
+	IsNotiBeforeAuction bool      `json:"is_noti_before_auction" query:"is_noti_before_auction" gorm:"type:bool"`
+	IsNotiDuringAuction bool      `json:"is_noti_during_auction" query:"is_noti_during_auction" gorm:"type:bool"`
+	IsNotiAfterAuction  bool      `json:"is_noti_after_auction" query:"is_noti_after_auction" gorm:"type:bool"`
+	IsUseTouchID        string    `json:"is_use_touch_id" query:"is_use_touch_id" gorm:"type:int(6);"`
+	CountWrongLogin     int       `json:"count_wrong_login" query:"count_wrong_login" gorm:"type:int(1);"`
+	LastLoginDate       string    `json:"last_login_date" query:"last_login_date" gorm:"type:varchar(15)"`
+	UserStatus          string    `json:"user_status" query:"user_status" gorm:"type:varchar(15)"`                   // ban approv
+	UserRegisterStatus  string    `json:"user_register_status" query:"user_register_status" gorm:"type:varchar(15)"` //สถานะหน้าสมัคร ถึงขันไหนละ 1 otp เสร็จ 2 กรอกข้อมูลเสร็จ 3 แอดมินแอพพรูฟ
+	RightDeposit        int       `json:"right_deposit" query:"right_deposit" gorm:"type:int(5)"`
+	IsApprove           bool      `json:"is_approve" query:"is_approve" gorm:"type:bool"`
+	ApproveBy           string    `json:"approve_by" query:"approve_by" gorm:"type:varchar(36);"`
+	ApproveAt           time.Time `json:"approve_at"`
 }
