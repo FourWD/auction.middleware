95049dc1a1a3bbf6f62757aaa8e5249dcc12f2ee
Pattanapong <pattanapong.amu@gmail.com>
2023-09-28 19:17:11 +0700

    Update bidding.go
    add bidding step for livelog

--- orm/bidding.go
+++ orm/bidding.go
@@ -14,6 +14,7 @@ type Bidding struct {
 	VehicleID        string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_bidding"`
 	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36);uniqueIndex:idx_bidding"`
 	Price            int       `json:"price" query:"price" gorm:"type:int"`
+	BiddingStep      int       `json:"bidding_step" query:"bidding_step" gorm:"type:int"`
 	IsProxy          bool      `json:"is_proxy" query:"is_proxy" gorm:"type:bool"`
 	Created          time.Time `json:"created" query:"created"`
 	CreatedUnixMilli int64     `json:"created_unix_milli" query:"created_unix_milli" gorm:"type:int"`
