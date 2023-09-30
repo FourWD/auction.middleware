d6f128fb406c62c6b09ab983dc272a6e4f6e4127
Pattanapong <pattanapong.amu@gmail.com>
2023-09-30 15:39:28 +0700

    Update finance.go
    add logo_home

--- orm/finance.go
+++ orm/finance.go
@@ -6,10 +6,11 @@ type Finance struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Logo    string `json:"logo" query:"logo" gorm:"type:varchar(255)"`
-	Code    string `json:"code" query:"code" gorm:"type:varchar(4)"`
-	Name    string `json:"name" query:"name" gorm:"type:varchar(50)"`
-	Color   string `json:"color" query:"color" gorm:"type:varchar(7)"`
-	LoanUrl string `json:"loan_url" query:"loan_url" gorm:"type:varchar(200)"`
+	Logo     string `json:"logo" query:"logo" gorm:"type:varchar(255)"`
+	LogoHome string `json:"logo_home" query:"logo_home" gorm:"type:varchar(255)"`
+	Code     string `json:"code" query:"code" gorm:"type:varchar(4)"`
+	Name     string `json:"name" query:"name" gorm:"type:varchar(50)"`
+	Color    string `json:"color" query:"color" gorm:"type:varchar(7)"`
+	LoanUrl  string `json:"loan_url" query:"loan_url" gorm:"type:varchar(200)"`
 	orm.RowOrder
 }
