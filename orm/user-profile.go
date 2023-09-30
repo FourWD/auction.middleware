f8c19e658f3c0da4c59a83043ea2f49af701797a
Pattanapong <pattanapong.amu@gmail.com>
2023-09-30 14:25:29 +0700

    update auction user

--- orm/user-profile.go
+++ orm/user-profile.go
@@ -10,6 +10,7 @@ type UserProfile struct {
 	CompanyRegisterTypeID string `json:"company_register_type_id" query:"company_register_type_id" gorm:"type:varchar(2);"`
 	BusinessTypeID        string `json:"business_type_id" query:"business_type_id" gorm:"type:varchar(2);"`
 	CompanyName           string `json:"company_name" query:"company_name" gorm:"type:varchar(50);"`
+	CompanyPhone          string `json:"company_phone" query:"company_phone" gorm:"type:varchar(50);"`
 	Tax                   string `json:"tax" query:"tax" gorm:"type:varchar(50);"`
 	HQShowroomName        string `json:"hq_show_room_name" query:"hq_show_room_name" gorm:"type:varchar(500);"`
 }
