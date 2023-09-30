4c5fe99860731ac15e4b98ef0d5d9bb7610dd944
puarns <puarns@gmail.com>
2023-09-30 11:26:22 +0700

    no message

--- orm/auction-vehicle-user-favorite.go
+++ orm/auction-vehicle-user-favorite.go
@@ -13,7 +13,7 @@ type AuctionVehicleUserFavorite struct {
 	AuctionID       string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
 	UserID          string    `json:"user_id" query:"user_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
 	VehicleID       string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
-	UserAuctionCode string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(15)"`
+	UserAuctionCode string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(5)"`
 	IsJoin          bool      `json:"is_join" query:"is_join" gorm:"bool"`
 	JoinDate        time.Time `json:"join_date" query:"join_date"`
 	IsFavorite      bool      `json:"is_favorite" query:"is_favorite" gorm:"bool"`
