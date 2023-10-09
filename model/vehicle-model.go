621b7a5b6f8672413c1d89aeee133a660a463847
p-amu <pattanapong.amu@gmail.com>
2023-10-09 13:31:11 +0700

    Update vehicle-model.go
    add for insert vehicle-model

--- model/vehicle-model.go
+++ model/vehicle-model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/FourWD/middleware/orm"
 )
 
@@ -8,36 +10,51 @@ type VehicleModel struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	SourceID               string `json:"source_id" query:"source_id" gorm:"type:varchar(36);"`
-	VehicleSubmodelID      string `json:"vehicle_submodel_id" query:"vehicle_submodel_id" gorm:"type:varchar(36);"`
-	VehicleColorID         string `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36);"`
-	ChassisNo              string `json:"chassis_no" query:"chassis_no" gorm:"type:varchar(20);"`
-	EngineNo               string `json:"engine_no" query:"engine_no" gorm:"type:varchar(20);"`
-	Mile                   string `json:"mile" query:"mile" gorm:"type:int(11);"`
-	YearManufacturing      string `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:int(4);"`
-	YearRegister           string `json:"year_register" query:"year_register" gorm:"type:int(4);"`
-	License                string `json:"license" query:"license" gorm:"type:varchar(10);"`
-	LicenseProviceID       string `json:"license_province_id" query:"license_province_id" gorm:"type:varchar(36);"`
-	VehicleGradeID         string `json:"vehicle_grade_id" query:"vehicle_grade_id" gorm:"type:varchar(36);"`
-	BranchID               string `json:"branch_id" query:"branch_id" gorm:"type:varchar(36)"`
-	ImagePreviewPath       string `json:"image_preview_path" query:"image_preview_path" gorm:"type:varchar(400)"`
-	CRPPrice               int    `json:"crp_price" query:"crp_price" gorm:"type:int"`
-	Remark                 string `json:"remark" query:"remark" gorm:"type:text;"`
-	SoldStatusID           int    `json:"sold_status_id" query:"sold_status_id" gorm:"type:int"`
-	DisplayRecommend       bool   `json:"display_recommend" query:"display_recommend" gorm:"type:bool"`
-	DisplaySpecialInterest bool   `json:"display_special_interest" query:"display_special_interest" gorm:"type:bool"`
-	EngineCapacity         int    `json:"engine_capacity" query:"engine_capacity" gorm:"type:int"`
-	EngineSize             int    `json:"engine_size" query:"engine_size" gorm:"type:int"`
-	EngineSizeActual       int    `json:"engine_size_actual" query:"engine_size_actual" gorm:"type:int"`
-	VehicleModelID         string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36);"`
-	VehicleDriveTypeID     string `json:"vehicle_drive_type_id" query:"vehicle_drive_type_id" gorm:"type:varchar(2);"`
-	VehicleGearID          string `json:"vehicle_gear_id" query:"vehicle_gear_id" gorm:"type:varchar(2);"`
-	VehicleFuelID          string `json:"vehicle_fuel_id" query:"vehicle_fuel_id" gorm:"type:varchar(2);"`
-	Seat                   int    `json:"seat" query:"seat" gorm:"type:int(2);"`
-	VehicleTypeID          string `json:"vehicle_type_id" query:"vehicle_type_id" gorm:"type:varchar(36);"`
-	VehicleBrandID         string `json:"vehicle_brand_id" query:"vehicle_brand_id" gorm:"type:varchar(36);"`
-	RegisterTypeCode       string `json:"register_type_code" query:"register_type_code" gorm:"type:varchar(36);"`
-	RegisterType           string `json:"register_type" query:"register_type" gorm:"type:varchar(36);"`
+	SourceID               string    `json:"source_id" query:"source_id" gorm:"type:varchar(36);"`
+	VehicleSubmodelID      string    `json:"vehicle_submodel_id" query:"vehicle_submodel_id" gorm:"type:varchar(36);"`
+	VehicleColorID         string    `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36);"`
+	ChassisNo              string    `json:"chassis_no" query:"chassis_no" gorm:"type:varchar(20);"`
+	EngineNo               string    `json:"engine_no" query:"engine_no" gorm:"type:varchar(20);"`
+	Mile                   string    `json:"mile" query:"mile" gorm:"type:int(11);"`
+	YearManufacturing      string    `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:int(4);"`
+	YearRegister           string    `json:"year_register" query:"year_register" gorm:"type:int(4);"`
+	License                string    `json:"license" query:"license" gorm:"type:varchar(10);"`
+	LicenseProviceID       string    `json:"license_province_id" query:"license_province_id" gorm:"type:varchar(36);"`
+	VehicleGradeID         string    `json:"vehicle_grade_id" query:"vehicle_grade_id" gorm:"type:varchar(36);"`
+	BranchID               string    `json:"branch_id" query:"branch_id" gorm:"type:varchar(36)"`
+	ImagePreviewPath       string    `json:"image_preview_path" query:"image_preview_path" gorm:"type:varchar(400)"`
+	CRPPrice               int       `json:"crp_price" query:"crp_price" gorm:"type:int"`
+	Remark                 string    `json:"remark" query:"remark" gorm:"type:text;"`
+	SoldStatusID           int       `json:"sold_status_id" query:"sold_status_id" gorm:"type:int"`
+	DisplayRecommend       bool      `json:"display_recommend" query:"display_recommend" gorm:"type:bool"`
+	DisplaySpecialInterest bool      `json:"display_special_interest" query:"display_special_interest" gorm:"type:bool"`
+	EngineCapacity         int       `json:"engine_capacity" query:"engine_capacity" gorm:"type:int"`
+	EngineSize             int       `json:"engine_size" query:"engine_size" gorm:"type:int"`
+	EngineSizeActual       int       `json:"engine_size_actual" query:"engine_size_actual" gorm:"type:int"`
+	VehicleModelID         string    `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36);"`
+	VehicleDriveTypeID     string    `json:"vehicle_drive_type_id" query:"vehicle_drive_type_id" gorm:"type:varchar(2);"`
+	VehicleGearID          string    `json:"vehicle_gear_id" query:"vehicle_gear_id" gorm:"type:varchar(2);"`
+	VehicleFuelID          string    `json:"vehicle_fuel_id" query:"vehicle_fuel_id" gorm:"type:varchar(2);"`
+	Seat                   int       `json:"seat" query:"seat" gorm:"type:int(2);"`
+	VehicleTypeID          string    `json:"vehicle_type_id" query:"vehicle_type_id" gorm:"type:varchar(36);"`
+	VehicleBrandID         string    `json:"vehicle_brand_id" query:"vehicle_brand_id" gorm:"type:varchar(36);"`
+	RegisterTypeCode       string    `json:"register_type_code" query:"register_type_code" gorm:"type:varchar(36);"`
+	RegisterType           string    `json:"register_type" query:"register_type" gorm:"type:varchar(36);"`
+	LicenseReceiveDate     string    `json:"license_receive_data" query:"license_receive_data" gorm:"type:varchar(20);"`
+	LicenseExpireDate      string    `json:"license_expire_data" query:"license_data_expire" gorm:"type:varchar(20);"`
+	TaxExpireDate          string    `json:"tax_expire_data" query:"license_data_expire" gorm:"type:varchar(20);"`
+	ContractNumber         string    `json:"contract_number" query:"contract_number" gorm:"type:varchar(20);"`
+	VehiclePickupDate      time.Time `json:"vehicle_pickup_date" query:"vehicle_pickup_date"`
+	VehicleAuctionReceipt  string    `json:"vehicle_auction_receipt" query:"vehicle_auction_receipt" gorm:"type:varchar(20);"` // เลขที่ใบรับรถขายทอดตลาด
+	Accessories            string    `json:"accessories" query:"accessories" gorm:"type:varchar(500);"`
+	PeriodOfUse            int       `json:"period_of_use" query:"period_of_use" gorm:"type:int(2);"`
+	Display                bool      `json:"display" query:"display" gorm:"type:bool"`
+	AdditionalInfo         string    `json:"additional_info" query:"additional_info" gorm:"type:text;"`
+	A3FirstName            string    `json:"a3_first_name" query:"a3_first_name" gorm:"type:varchar(50);"`
+	A3LastName             string    `json:"a3_last_name" query:"a3_last_name" gorm:"type:varchar(50);"`
+	A3CarOwnerNumber       int       `json:"a3_car_owner_number" query:"a3_car_owner_number" gorm:"type:int(2);"`
+	A3AccidentHistory      string    `json:"a3_accident_history" query:"a3_accident_history" gorm:"type:varchar(50);"`
+	A3VehicleLienExists    string    `json:"a3_vehicle_lien_exists" query:"a3_vehicle_lien_exists" gorm:"type:varchar(50);"`
 
 	// ImgStrFront        string `json:"img_str_front" query:"img_str_front" gorm:"type:varchar(400)"`
 	// ImgStrBack         string `json:"img_str_back" query:"img_str_back" gorm:"type:varchar(400)"`
