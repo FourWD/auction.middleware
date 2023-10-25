53159d857c3eff5e4e276f4ba5299a10c5a9d300
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-10-25 11:29:05 +0700

    update bank-transfer

--- orm/bank-transfer.go
+++ orm/bank-transfer.go
@@ -10,24 +10,25 @@ type BankTransfer struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	AuctionID        string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
-	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
-	TitleID          string    `json:"title_id" query:"title_id" gorm:"type:varchar(20);"`
-	PrefixID         string    `json:"prefix_id" query:"prefix_id" gorm:"type:varchar(2);"` //จาก int เป็น string
-	Firstname        string    `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
-	Lastname         string    `json:"lastname" query:"lastname" gorm:"type:varchar(100);"`
-	IdcardNo         string    `json:"idcard_no" query:"idcard_no" gorm:"type:varchar(13)"`
-	Telephone        string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
-	FileIdcardID     string    `json:"file_idcard_id" query:"file_idcard_id" gorm:"type:varchar(36)"`
-	TransferDateTime time.Time `json:"transfer_date_time" query:"transfer_date_time"`
-	Amount           int       `json:"amount" query:"amount" gorm:"type:int"`
-	FileSlipID       string    `json:"file_slip_id" query:"file_slip_id" gorm:"type:varchar(36)"`
-	AcceptTCVersion  string    `json:"accept_tc_version" query:"accept_tc_version" gorm:"type:varchar(36)"`
-	BankTransferID   string    `json:"bank_transfer_id" query:"bank_transfer_id" gorm:"type:varchar(2)"`
-	ApproveBy        string    `json:"approve_by" query:"approve_by" gorm:"type:varchar(36)"`
-	ApproveDate      time.Time `json:"approve_date" query:"approve_date"`
-	IsMobileBanking  bool      `json:"is_mobile_banking" query:"is_mobile_banking" gorm:"type:bool"`
-	OcupationID      string    `json:"ocupation_id" query:"ocupation_id" gorm:"type:varchar(36)"`
+	AuctionID string `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
+	UserID    string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	// TitleID         string    `json:"title_id" query:"title_id" gorm:"type:varchar(20);"` //
+	PrefixID        string    `json:"prefix_id" query:"prefix_id" gorm:"type:varchar(2);"` //จาก int เป็น string
+	Firstname       string    `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
+	Lastname        string    `json:"lastname" query:"lastname" gorm:"type:varchar(100);"`
+	IdcardNo        string    `json:"idcard_no" query:"idcard_no" gorm:"type:varchar(13)"`
+	Telephone       string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
+	FileIdcardID    string    `json:"file_idcard_id" query:"file_idcard_id" gorm:"type:varchar(36)"`
+	TransferDate    time.Time `json:"transfer_date" query:"transfer_date"`
+	Amount          int       `json:"amount" query:"amount" gorm:"type:int"`
+	FileSlipID      string    `json:"file_slip_id" query:"file_slip_id" gorm:"type:varchar(36)"`
+	AcceptTCVersion string    `json:"accept_tc_version" query:"accept_tc_version" gorm:"type:varchar(36)"`
+	BankTransferID  string    `json:"bank_transfer_id" query:"bank_transfer_id" gorm:"type:varchar(2)"`
+	ApproveBy       string    `json:"approve_by" query:"approve_by" gorm:"type:varchar(36)"`
+	ApproveDate     time.Time `json:"approve_date" query:"approve_date"`
+	IsMobileBanking bool      `json:"is_mobile_banking" query:"is_mobile_banking" gorm:"type:bool"`
+	OcupationID     string    `json:"ocupation_id" query:"ocupation_id" gorm:"type:varchar(36)"`
+	EffectiveDate   time.Time `json:"effective_date" query:"effective_date"`
 
 	Address       string `json:"address" query:"address" gorm:"type:text"`
 	Street        string `json:"street" query:"street" gorm:"type:varchar(200)"`
