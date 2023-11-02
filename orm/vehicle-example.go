406abb152db64e9b0f450e0c2e2e13d8bcc38387
Pattanapong <pattanapong.amu@gmail.com>
2023-11-02 19:25:14 +0700

    change vehicle-example

---
+++ orm/vehicle-example.go
@@ -0,0 +1,11 @@
+package orm
+
+import "github.com/FourWD/auction.middleware/model"
+
+type VehicleExample struct {
+	model.VehicleModel
+}
+
+// func (Vehicle) TableName() string {
+// 	return "vehicles"
+// }
