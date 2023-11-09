5d3d37417531b4e5062e41b6790d3c4cf3f6aa03
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-11-09 15:46:09 +0700

    update file gorm

--- orm/file.go
+++ orm/file.go
@@ -1,14 +1,12 @@
 package orm
 
 import (
-	"time"
+	"github.com/FourWD/middleware/orm"
 )
 
 type File struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
-
-	CreatedAt time.Time `json:"created_at" query:"created_at" gorm:"<-:create"`
-	CreatedBy string    `json:"created_by" query:"created_by" gorm:"type:varchar(36)"`
+	orm.GormModel
 
 	FileName   string `json:"file_name" query:"file_name" gorm:"type:varchar(100)"`
 	Path       string `json:"path" query:"path" gorm:"type:varchar(500)"`
