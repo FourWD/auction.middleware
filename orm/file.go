76c3ffa6e3e59b73e860d245c2ab3ba86362ec40
Pattanapong <pattanapong.amu@gmail.com>
2023-11-09 15:58:03 +0700

    Update file.go
    add field extension

--- orm/file.go
+++ orm/file.go
@@ -9,6 +9,7 @@ type File struct {
 	orm.GormModel
 
 	FileName   string `json:"file_name" query:"file_name" gorm:"type:varchar(100)"`
+	Extension  string `json:"extension" query:"extension" gorm:"type:varchar(5)"`
 	Path       string `json:"path" query:"path" gorm:"type:varchar(500)"`
 	FullPath   string `json:"full_path"  query:"full_path" gorm:"type:varchar(500)"`
 	Cdn        string `json:"cdn" query:"cdn" gorm:"type:varchar(100)"`
