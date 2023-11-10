093fda6749ae1332711414637fe46f32fa9533cf
Pattanapong <pattanapong.amu@gmail.com>
2023-11-10 19:07:07 +0700

    update bank list
    bank list to bank jame

--- orm/bank.go
+++ orm/bank.go
@@ -2,7 +2,7 @@ package orm
 
 import "github.com/FourWD/middleware/orm"
 
-type BankList struct {
+type Bank struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
