8c86eef97724f6878f96058a6dde476f93b6ee97
puarns <puarns@gmail.com>
2023-11-16 12:26:45 +0700

    add view "vehicle_year_groups"

--- orm/year-group.go
+++ orm/year-group.go
@@ -9,3 +9,8 @@ type YearGroup struct {
 	Name     string `json:"name" query:"name" gorm:"type:varchar(100)"`
 	YearList string `json:"year_list" query:"year_list" gorm:"type:varchar(200)"`
 }
+
+/*
+view "vehicle_year_groups"
+select v.id as vehicle_id, v.year_manufacturing, y.id as year_group_id, y.name as year_group_name from vehicles v
+LEFT JOIN year_groups y ON y.year_list LIKE CONCAT('%', v.year_manufacturing ,'%'); */
