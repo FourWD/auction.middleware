d9cb9e07160dfef4dec8d8ed545a7c5ff7816a5b
winra159@gmail.com <winra159@gmail.com>
2023-11-21 10:28:35 +0700

    add detail,remark

--- orm/finance.go
+++ orm/finance.go
@@ -12,6 +12,9 @@ type Finance struct {
 	Name             string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	Color            string `json:"color" query:"color" gorm:"type:varchar(7)"`
 	LoanDiscription  string `json:"loan_discription" query:"loan_discription" gorm:"type:text"`
+	Detail           string `json:"detail" query:"detail" gorm:"type:text"`
+	Remark           string `json:"remark" query:"remark" gorm:"type:text"`
 	IsRegisterActive bool   `json:"is_register_active" query:"is_register_active" gorm:"type:bool"`
+
 	orm.GormRowOrder
 }
