7480d00db00cc609e81593d6f15679851f9ffc2a
puarns <puarns@gmail.com>
2023-11-24 02:49:43 +0700

    no message

--- orm/proxy.go
+++ orm/proxy.go
@@ -5,14 +5,11 @@ import (
 )
 
 type Proxy struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
-
-	AuctionID string `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
-	VehicleID string `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
-	Price     int    `json:"price" query:"price" gorm:"type:int;uniqueIndex:idx_auction_id_vehicle_id_price"`
-
-	UserID string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
-
+	ID               string    `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
+	AuctionID        string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
+	VehicleID        string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
+	Price            int       `json:"price" query:"price" gorm:"type:int;uniqueIndex:idx_auction_id_vehicle_id_price"`
+	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
 	Created          time.Time `json:"created" query:"created"`
 	CreatedUnixMilli int64     `json:"created_unix_milli" query:"created_unix_milli" gorm:"type:int"`
 }
