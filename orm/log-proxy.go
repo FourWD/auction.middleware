002602e25b51492c98f4cfa8769d3b31b864526d
puarns <puarns@gmail.com>
2023-11-23 23:46:34 +0700

    new price

--- orm/log-proxy.go
+++ orm/log-proxy.go
@@ -9,8 +9,8 @@ type LogProxy struct {
 
 	AuctionID string `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
 	VehicleID string `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
-	FromPrice int    `json:"from_price" query:"from_price" gorm:"type:int"`
-	ToPrice   int    `json:"to_price" query:"to_price" gorm:"type:int"`
+	Price     int    `json:"price" query:"price" gorm:"type:int"`
+	NewPrice  int    `json:"new_price" query:"new_price" gorm:"type:int"`
 	IsDelete  bool   `json:"is_delete" query:"is_delete" gorm:"type:bool"`
 
 	UserID string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
