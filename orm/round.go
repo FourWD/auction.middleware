4dd97f374785047900c6f10f7bd86ad1d3b8f13a
winra159@gmail.com <winra159@gmail.com>
2023-11-29 10:04:31 +0700

    m change name 200

--- orm/round.go
+++ orm/round.go
@@ -11,7 +11,7 @@ type Round struct {
 	Logo     string `json:"logo" query:"logo" gorm:"type:varchar(255)"`
 	LogoHome string `json:"logo_home" query:"logo_home" gorm:"type:varchar(255)"`
 	Code     string `json:"code" query:"code" gorm:"type:varchar(4)"`
-	Name     string `json:"name" query:"name" gorm:"type:varchar(50)"`
+	Name     string `json:"name" query:"name" gorm:"type:varchar(200)"`
 	Color    string `json:"color" query:"color" gorm:"type:varchar(7)"`
 
 	model.GormRowOrder
