1280f85e070c9cb11680f7b6943e3dadad5e021f
winra159@gmail.com <winra159@gmail.com>
2023-12-18 14:01:12 +0700

    change pickup name

--- orm/auction.go
+++ orm/auction.go
@@ -44,7 +44,7 @@ type Auction struct {
 	RunningNo int `json:"running_no" query:"running_no" gorm:"primary_key;auto_increment;not_null"`
 
 	CountOfSedan  int `json:"count_of_sedan" query:"count_of_sedan" gorm:"type:int(6)"`
-	CountOfPickUp int `json:"count_of_pick_up" query:"count_of_pick_up" gorm:"type:int(6)"`
+	CountOfPickUp int `json:"count_of_pickup" query:"count_of_pickup" gorm:"type:int(6)"`
 	CountOfSUV    int `json:"count_of_suv" query:"count_of_suv" gorm:"type:int(6)"`
 	CountOfVan    int `json:"count_of_van" query:"count_of_van" gorm:"type:int(6)"`
 	CountOfTruck  int `json:"count_of_truck" query:"count_of_truck" gorm:"type:int(6)"`
