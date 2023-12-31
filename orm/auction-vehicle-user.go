e30c91e5961e067b3db3cfeb021df61a835796fc
puarns <puarns@gmail.com>
2023-12-31 17:15:42 +0700

    no message

--- orm/auction-vehicle-user.go
+++ orm/auction-vehicle-user.go
@@ -8,8 +8,8 @@ type AuctionVehicleUser struct {
 	ID uint `json:"id" query:"id" gorm:"primaryKey;autoIncrement"`
 
 	AuctionID       string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
-	UserID          string    `json:"user_id" query:"user_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
 	VehicleID       string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
+	UserID          string    `json:"user_id" query:"user_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_user_id"`
 	UserAuctionCode string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(5)"`
 	IsJoin          bool      `json:"is_join" query:"is_join" gorm:"bool"`
 	JoinDate        time.Time `json:"join_date" query:"join_date"`
--- orm/proxy.go
+++ orm/proxy.go
@@ -10,6 +10,7 @@ type Proxy struct {
 	VehicleID        string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
 	Price            int       `json:"price" query:"price" gorm:"type:int;uniqueIndex:idx_auction_id_vehicle_id_price"`
 	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	UserAuctionCode  string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(5)"`
 	Created          time.Time `json:"created" query:"created"`
 	CreatedUnixMilli int64     `json:"created_unix_milli" query:"created_unix_milli" gorm:"type:int"`
 }
