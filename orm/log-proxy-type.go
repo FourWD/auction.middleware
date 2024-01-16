7a90b816f78fcb009d50d7c39fb897782600444e
puarns <puarns@gmail.com>
2024-01-16 08:21:01 +0700

    no message

--- orm/log-proxy-type.go
+++ orm/log-proxy-type.go
@@ -1,7 +1,6 @@
 package orm
 
 type LogProxyType struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(1);primary_key"`
-
+	ID   string `json:"id" query:"id" gorm:"type:varchar(1);primary_key"`
 	Name string `json:"name" query:"name" gorm:"type:varchar(50)"`
 }
