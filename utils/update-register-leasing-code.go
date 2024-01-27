48a08f39a482cb15ba28f29c3b9a25a51c155734
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-01-27 18:24:26 +0700

    ++

--- utils/update-register-leasing-code.go
+++ utils/update-register-leasing-code.go
@@ -9,6 +9,174 @@ import (
 	"github.com/FourWD/middleware/common"
 )
 
+func GenCode(userID string) (string, error) {
+
+	type CustomerCode struct {
+		orm.User
+		UserTypeCode string `json:"user_type_code" query:"user_type_code"`
+	}
+	var user CustomerCode
+	sql := fmt.Sprintf(`SELECT users.running_no, users.created_at, users.user_type_id, user_types.code as user_type_code
+							FROM users
+						LEFT JOIN user_types ON users.user_type_id = user_types.id
+						WHERE users.id = '%s' AND users.user_type_id IS NOT NULL AND users.user_type_id != ''`, userID)
+
+	common.Database.Raw(sql).Scan(&user)
+	common.Print(sql, "ssss")
+	common.Print("struct", common.StructToString(user))
+
+	type Count struct {
+		Count int64 `json:"count"`
+	}
+
+	var count Count
+	year := time.Now().Year()
+	month := time.Now().Month()
+	// day := time.Now().Day()
+
+	query := fmt.Sprintf(`SELECT COUNT(*) FROM users WHERE YEAR(created_at) = %d AND MONTH(created_at) = %d
+	AND running_no <= '%d' AND user_type_id = '%s'`, year, month, user.RunningNo, user.UserTypeID)
+	common.Database.Raw(query).Scan(&count.Count)
+	common.Print(query, fmt.Sprintf("%d", count.Count))
+
+	fyear := strconv.Itoa(year)
+	fmonth := fmt.Sprintf("%02d", month)
+	// fday := fmt.Sprintf("%02d", day)
+	code := fmt.Sprintf("%s%s%s%04d", user.UserTypeCode, fyear[2:4], fmonth, count.Count)
+
+	update := fmt.Sprintf(`UPDATE users set code = '%s' where id = '%s'`, code, userID)
+	checkupdate := common.Database.Exec(update)
+	if checkupdate.RowsAffected == 0 {
+		common.PrintError("Error checkupdate", userID)
+	}
+	common.Print("check", code)
+	return code, nil
+}
+func UpdateBanktrasferCode(userID string) (string, error) {
+	type Code struct {
+		orm.BankTransfer
+		Code string `json:"code" query:"code"`
+	}
+
+	var verifyCode Code
+
+	sql := fmt.Sprintf(`SELECT u.id,p.code, bt.running_no FROM users u 
+							LEFT JOIN payment_types p ON u.payment_type_id = p.id
+							LEFT JOIN bank_transfers bt ON u.id = bt.user_id
+						WHERE u.id = '%s'`, userID)
+
+	common.Database.Raw(sql).Scan(&verifyCode)
+
+	type Count struct {
+		Count int64 `json:"count"`
+	}
+
+	var count Count
+	year := time.Now().Year()
+	day := time.Now().Day()
+
+	queryBanktransfer := fmt.Sprintf(`SELECT COUNT(*) FROM bank_transfers WHERE YEAR(created_at) = %d AND DAY(created_at) = %d `, year, day)
+	common.Database.Raw(queryBanktransfer).Scan(&count.Count)
+	common.Print(queryBanktransfer, fmt.Sprintf("%d", count.Count))
+
+	fyear := strconv.Itoa(year)
+	fday := fmt.Sprintf("%02d", day)
+
+	verifyBanktransfer := fmt.Sprintf("%s%s%s%03d", verifyCode.Code, fyear[2:4], fday, count.Count)
+	updateBanktransfer := fmt.Sprintf(`UPDATE bank_transfers SET code = '%s' WHERE id = '%s'`, verifyBanktransfer, userID)
+
+	checkUpdate := common.Database.Exec(updateBanktransfer)
+
+	if checkUpdate.RowsAffected == 0 {
+		common.PrintError("Error checkupdate", userID)
+	}
+
+	return verifyBanktransfer, nil
+}
+func UpdateRefundCode(userID string) (string, error) {
+	type Code struct {
+		orm.Refund
+		Code string `json:"code" query:"code"`
+	}
+
+	var verifyCode Code
+
+	sql := fmt.Sprintf(`SELECT u.id,p.code, rf.running_no FROM users u 
+							LEFT JOIN payment_types p ON u.payment_type_id = p.id
+							LEFT JOIN refunds rf ON u.id = rf.user_id
+						WHERE u.id = '%s'`, userID)
+
+	common.Database.Raw(sql).Scan(&verifyCode)
+
+	type Count struct {
+		Count int64 `json:"count"`
+	}
+
+	var count Count
+	year := time.Now().Year()
+	day := time.Now().Day()
+
+	queryRefund := fmt.Sprintf(`SELECT COUNT(*) FROM refunds WHERE YEAR(created_at) = %d AND DAY(created_at) = %d `, year, day)
+	common.Database.Raw(queryRefund).Scan(&count.Count)
+	common.Print(queryRefund, fmt.Sprintf("%d", count.Count))
+
+	fyear := strconv.Itoa(year)
+	fday := fmt.Sprintf("%02d", day)
+
+	verifyRefund := fmt.Sprintf("%s%s%s%03d", "RE", fyear[2:4], fday, count.Count)
+	updateRefund := fmt.Sprintf(`UPDATE refunds SET code = '%s' WHERE id = '%s'`, verifyRefund, userID)
+
+	checkUpdate := common.Database.Exec(updateRefund)
+
+	if checkUpdate.RowsAffected == 0 {
+		common.PrintError("Error checkupdate", userID)
+	}
+
+	return verifyRefund, nil
+}
+
+func UpdateDeductCode(userID string) (string, error) {
+	type Code struct {
+		orm.DeductRight
+		Code string `json:"code" query:"code"`
+	}
+
+	var verifyCode Code
+
+	sql := fmt.Sprintf(`SELECT u.id,p.code, dr.running_no FROM users u 
+							LEFT JOIN payment_types p ON u.payment_type_id = p.id
+							LEFT JOIN deduct_rights dr ON u.id = dr.user_id
+						WHERE u.id = '%s'`, userID)
+
+	common.Database.Raw(sql).Scan(&verifyCode)
+
+	type Count struct {
+		Count int64 `json:"count"`
+	}
+
+	var count Count
+	year := time.Now().Year()
+	day := time.Now().Day()
+
+	queryRefund := fmt.Sprintf(`SELECT COUNT(*) FROM refunds WHERE YEAR(created_at) = %d AND DAY(created_at) = %d `, year, day)
+	common.Database.Raw(queryRefund).Scan(&count.Count)
+	common.Print(queryRefund, fmt.Sprintf("%d", count.Count))
+
+	fyear := strconv.Itoa(year)
+	fday := fmt.Sprintf("%02d", day)
+
+	verifyDeduct := fmt.Sprintf("%s%s%s%03d", "DE", fyear[2:4], fday, count.Count)
+	updateDeduct := fmt.Sprintf(`UPDATE deduct_rights SET code = '%s' WHERE id = '%s'`, verifyDeduct, userID)
+
+	checkUpdate := common.Database.Exec(updateDeduct)
+
+	if checkUpdate.RowsAffected == 0 {
+		common.PrintError("Error checkupdate", userID)
+	}
+
+	return verifyDeduct, nil
+}
+
 func UpdateRegisterLeasingCode(id string) (string, error) {
 
 	type Code struct {
