ce1897a16cd24fa71a0e122ef8fd174fbcd14f6f
p-amu <99467582+p-amu@users.noreply.github.com>
2024-01-31 03:43:09 +0700

    Update user-profile.go
    group file for easy read

--- orm/user-profile.go
+++ orm/user-profile.go
@@ -15,11 +15,20 @@ type UserProfile struct {
 	BankAccountName       string `json:"refund_bank_account_name" query:"refund_bank_account_name" gorm:"type:varchar(15)"`
 	BankAccountNo         string `json:"refund_bank_account_no" query:"refund_bank_account_no" gorm:"type:varchar(15)"`
 
-	FileCompanyRegisterID  string `json:"file_company_register_id" query:"file_company_register_id" gorm:"type:varchar(36)"`
-	FilePP20ID             string `json:"file_pp20_id" query:"file_pp20_id" gorm:"type:varchar(36)"`
-	FileIdcardID           string `json:"file_idcard_id" query:"file_idcard_id" gorm:"type:varchar(36)"`
+	// register all
+	FileIdcardID string `json:"file_idcard_id" query:"file_idcard_id" gorm:"type:varchar(36)"`
+
+	// customer juristic and tent
+	FileCompanyRegisterID string `json:"file_company_register_id" query:"file_company_register_id" gorm:"type:varchar(36)"`
+	FilePP20ID            string `json:"file_pp20_id" query:"file_pp20_id" gorm:"type:varchar(36)"` // ใบทะเบียนภาษีมูลค่าเพิ่ม ภ.พ.20
+
+	// register leasing
 	FileHouseParticularsID string `json:"file_house_particulars_id" query:"file_house_particulars_id" gorm:"type:varchar(36)"`
-	FileBookbankID         string `json:"file_bookbank_id" query:"file_bookbank_id" gorm:"type:varchar(36)"`
-	FileAttorneyLetterID   string `json:"file_attorney_letter_id" query:"file_attorney_letter_id" gorm:"type:varchar(36)"`
-	FileAttorneyIdcardID   string `json:"file_attorney_idcard_id" query:"file_attorney_idcard_id" gorm:"type:varchar(36)"`
+	FilePayslipID          string `json:"file_payslip_id" query:"file_payslip_id" gorm:"type:varchar(36)"`
+
+	FileAttorneyLetterID string `json:"file_attorney_letter_id" query:"file_attorney_letter_id" gorm:"type:varchar(36)"`
+	FileAttorneyIdcardID string `json:"file_attorney_idcard_id" query:"file_attorney_idcard_id" gorm:"type:varchar(36)"`
+
+	// refund
+	FileBookbankID string `json:"file_bookbank_id" query:"file_bookbank_id" gorm:"type:varchar(36)"`
 }
