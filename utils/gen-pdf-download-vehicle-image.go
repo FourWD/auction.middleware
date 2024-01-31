ce295230b2b5d64664bbfef08dd1bd3a9e7e2445
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-01-31 10:44:04 +0700

    update utils

---
+++ utils/gen-pdf-download-vehicle-image.go
@@ -0,0 +1,15 @@
+package utils
+
+func GenPDFDownloadVehicleImage(auctionID string, userID string) (string, error) {
+	return "https://storage.googleapis.com/fourwd-auction/app/example_file/Payment-Term-Customer.pdf", nil
+}
+
+func GenPDFVehicle(auctionID string, userID string) (string, error) { //carlist
+	return "https://storage.googleapis.com/fourwd-auction/app/example_file/Payment-Term-Customer.pdf", nil
+}
+func GenPDFAuctionResult(auctionID string) (string, error) {
+	return "https://storage.googleapis.com/fourwd-auction/app/example_file/Payment-Term-Customer.pdf", nil
+}
+func GenPDFPayment(auctionID string, userID string) (string, error) {
+	return "https://storage.googleapis.com/fourwd-auction/app/example_file/Payment-Term-Customer.pdf", nil
+}
