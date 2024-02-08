24ff5bbbf0c007ac52c74577c73575b2007bff5a
puarns <puarns@gmail.com>
2024-02-08 14:52:59 +0700

    change proxy milli -> nano

--- orm/log-proxy.go
+++ orm/log-proxy.go
@@ -7,12 +7,12 @@ import (
 type LogProxy struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 
-	AuctionID        string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
-	VehicleID        string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
-	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
-	Price            int       `json:"price" query:"price" gorm:"type:int"`
-	NewPrice         int       `json:"new_price" query:"new_price" gorm:"type:int"`
-	LogProxyTypeID   string    `json:"log_proxy_type_id" query:"log_proxy_type_id" gorm:"type:varchar(1)"`
-	Created          time.Time `json:"created" query:"created"`
-	CreatedUnixMilli int64     `json:"created_unix_milli" query:"created_unix_milli" gorm:"type:int"`
+	AuctionID       string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36)"`
+	VehicleID       string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
+	UserID          string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	Price           int       `json:"price" query:"price" gorm:"type:int"`
+	NewPrice        int       `json:"new_price" query:"new_price" gorm:"type:int"`
+	LogProxyTypeID  string    `json:"log_proxy_type_id" query:"log_proxy_type_id" gorm:"type:varchar(1)"`
+	Created         time.Time `json:"created" query:"created"`
+	CreatedUnixNano int64     `json:"created_unix_nano" query:"created_unix_nano" gorm:"type:int"`
 }
--- orm/proxy.go
+++ orm/proxy.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Proxy struct {
-	ID               string    `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
-	AuctionID        string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
-	VehicleID        string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
-	Price            int       `json:"price" query:"price" gorm:"type:int;uniqueIndex:idx_auction_id_vehicle_id_price"`
-	UserID           string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
-	UserAuctionCode  string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(5)"`
-	Created          time.Time `json:"created" query:"created"`
-	CreatedUnixMilli int64     `json:"created_unix_milli" query:"created_unix_milli" gorm:"type:int"`
-	IsProcess        bool      `json:"is_process" query:"is_process" gorm:"type:bool"`
-	ProcessDate      time.Time `json:"process_date" query:"process_date"`
-	IsWin            bool      `json:"is_win" query:"is_win" gorm:"type:bool"`
-	IsClear          bool      `json:"is_clear" query:"is_clear" gorm:"type:bool"`
+	ID              string    `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
+	AuctionID       string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
+	VehicleID       string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);uniqueIndex:idx_auction_id_vehicle_id_price"`
+	Price           int       `json:"price" query:"price" gorm:"type:int;uniqueIndex:idx_auction_id_vehicle_id_price"`
+	UserID          string    `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	UserAuctionCode string    `json:"user_auction_code" query:"user_auction_code" gorm:"type:varchar(5)"`
+	Created         time.Time `json:"created" query:"created"`
+	CreatedUnixNano int64     `json:"created_unix_nano" query:"created_unix_nano" gorm:"type:int"`
+	IsProcess       bool      `json:"is_process" query:"is_process" gorm:"type:bool"`
+	ProcessDate     time.Time `json:"process_date" query:"process_date"`
+	IsWin           bool      `json:"is_win" query:"is_win" gorm:"type:bool"`
+	IsClear         bool      `json:"is_clear" query:"is_clear" gorm:"type:bool"`
 }
