a71dfb01905e474d624fd143ba527cbe8a6a8268
puarns <puarns@gmail.com>
2024-02-26 12:24:45 +0700

    no message

--- orm/template-dowload-vehicle-image.go
+++ orm/template-dowload-vehicle-image.go
@@ -4,7 +4,7 @@ import (
 	"github.com/FourWD/middleware/model"
 )
 
-type TemplateDowloadVehicleImage struct {
+type TemplateDownloadVehicleImage struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
