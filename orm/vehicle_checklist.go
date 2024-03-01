124afc798c5808c134ca892d0848f7bcc96fc36b
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 14:36:21 +0700

    Create vehicle_checklist.go

---
+++ orm/vehicle_checklist.go
@@ -0,0 +1,15 @@
+package orm
+
+import (
+	"github.com/FourWD/middleware/model"
+)
+
+type VehicleChecklist struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
+
+	model.GormModel
+
+	VehicleID       string `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
+	FormCheckListID string `json:"form_checklist_id" query:"form_checklist_id" gorm:"type:varchar(36)"`
+	IsCheck         int    `json:"is_check" query:"is_check" gorm:"type:int"`
+}
