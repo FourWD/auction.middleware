b6ff58e80435955c4b6aaa506dba6e5416ed6660
puarns <puarns@gmail.com>
2024-03-05 21:31:17 +0700

    no message

--- orm/vehicle-grade-remark.go
+++ orm/vehicle-grade-remark.go
@@ -1,6 +1,11 @@
 package orm
 
+import "time"
+
 type VehicleGradeRemark struct {
-	ChassisNumber string `json:"chassis_number" query:"chassis_number" gorm:"type:varchar(20)"`
-	GradeRemark   string `json:"grade_remark" query:"grade_remark" gorm:"type:text"`
+	ChassisNumber string    `json:"chassis_number" query:"chassis_number" gorm:"type:varchar(30);primary_key"`
+	License       string    `json:"license" query:"license" gorm:"type:varchar(10)"`
+	GradeRemark   string    `json:"grade_remark" query:"grade_remark" gorm:"type:text"`
+	Grade         string    `json:"grade" query:"grade" gorm:"type:varchar(2)"`
+	UpdatedAt     time.Time `json:"updated_at" query:"updated_at"`
 }
