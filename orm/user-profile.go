6da11d9e86936be80a352fabc1b825288facfea6
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-20 15:59:48 +0700

    tax
    tax address

--- orm/user-profile.go
+++ orm/user-profile.go
@@ -7,4 +7,6 @@ type UserProfile struct {
 
 	FileAttorneyLetterID string `json:"file_attorney_letter_id" query:"file_attorney_letter_id" gorm:"type:varchar(36)"`
 	FileAttorneyIdcardID string `json:"file_attorney_idcard_id" query:"file_attorney_idcard_id" gorm:"type:varchar(36)"`
+
+	IsAddressTax bool `json:"is_address_tax" query:"is_address_tax" gorm:"type:bool"` //สำหรับ ติ๊ก
 }
