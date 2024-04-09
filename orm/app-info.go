e8b0bb4fc1146c82804fbdd279994f9c132b4cf1
p-amu <99467582+p-amu@users.noreply.github.com>
2024-04-09 11:57:20 +0700

    Update app-info.go

--- orm/app-info.go
+++ orm/app-info.go
@@ -6,7 +6,8 @@ type AppInfo struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	AppVersion string `json:"app_version" query:"button_url" gorm:"type:varchar(20)"`
+	AppEnv     string `json:"app_env" query:"app_env" gorm:"type:varchar(20)"`
+	AppVersion string `json:"app_version" query:"app_version" gorm:"type:varchar(20)"`
 	AppUrl     string `json:"app_url" query:"app_url" gorm:"type:varchar(500)"`
 	IsLasted   bool   `json:"is_lasted" query:"is_lasted" gorm:"type:bool"`
 }
