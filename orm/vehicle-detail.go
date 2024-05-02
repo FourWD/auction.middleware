68d494a75735a1ce03e5db21631e831bc519dc41
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-02 11:02:50 +0700

    Update vehicle-detail.go

--- orm/vehicle-detail.go
+++ orm/vehicle-detail.go
@@ -6,5 +6,5 @@ type VehicleDetail struct {
 	VehicleID string `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	SoldStatusID string `json:"sold_status_id" query:"sold_status_id" gorm:"type:varchar(2)"`
+	VehicleSoldStatusID string `json:"vehicle_sold_status_id" query:"vehicle_sold_status_id" gorm:"type:varchar(2)"`
 }
