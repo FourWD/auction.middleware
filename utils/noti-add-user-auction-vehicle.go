ae21a3f8f55f7864178f94ffbcacb8e0441d7902
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-10 09:28:30 +0700

    Update noti-add-user-auction-vehicle.go

--- utils/noti-add-user-auction-vehicle.go
+++ utils/noti-add-user-auction-vehicle.go
@@ -7,12 +7,14 @@ import (
 )
 
 func NotiAddUserAuctionVehicle(notiToken string, auctionID string, vehicleID string) error {
-
+	fmt.Println(notiToken)
 	topicAuction := fmt.Sprintf("JOIN_AUCTION_%s", auctionID)
+	fmt.Println(topicAuction)
 	if err := common.AddUserToSubscription(notiToken, topicAuction); err != nil {
 		return err
 	}
 
 	topicAuctionVechile := fmt.Sprintf("JOIN_AUCTION_VEHICLE_%s_%s", auctionID, vehicleID)
+	fmt.Println(topicAuctionVechile)
 	return common.AddUserToSubscription(notiToken, topicAuctionVechile)
 }
