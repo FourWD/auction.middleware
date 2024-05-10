87d32fa9cb10e9d08c0c17221b717711c331ab64
Plew Woo <pew_woothiphom@hotmail.com>
2024-05-10 10:31:22 +0700

    Update noti-add-user-auction-vehicle.go

--- utils/noti-add-user-auction-vehicle.go
+++ utils/noti-add-user-auction-vehicle.go
@@ -1,20 +1,25 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/FourWD/middleware/common"
 )
 
 func NotiAddUserAuctionVehicle(notiToken string, auctionID string, vehicleID string) error {
-	fmt.Println(notiToken)
+	if notiToken == "" || auctionID == "" || vehicleID == "" {
+		common.PrintError("NotiAddUserAuctionVehicle", "some value is empty notiToken : "+notiToken+" auctionID : "+auctionID+" vehicleID : "+vehicleID)
+		return errors.New("some value is empty")
+	}
+	common.Print("notiToken", notiToken)
 	topicAuction := fmt.Sprintf("JOIN_AUCTION_%s", auctionID)
-	fmt.Println(topicAuction)
+	common.Print("topicAuction", topicAuction)
 	if err := common.AddUserToSubscription(notiToken, topicAuction); err != nil {
 		return err
 	}
 
 	topicAuctionVechile := fmt.Sprintf("JOIN_AUCTION_VEHICLE_%s_%s", auctionID, vehicleID)
-	fmt.Println(topicAuctionVechile)
+	common.Print("topicAuctionVechile", topicAuctionVechile)
 	return common.AddUserToSubscription(notiToken, topicAuctionVechile)
 }
