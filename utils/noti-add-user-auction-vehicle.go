ebd832924cd54b4aa145af2055c33830f91e6881
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-05-24 10:38:31 +0700

    Update noti-add-user-auction-vehicle.go

--- utils/noti-add-user-auction-vehicle.go
+++ utils/noti-add-user-auction-vehicle.go
@@ -15,13 +15,39 @@ func NotiAddUserAuctionVehicle(notiToken string, auctionID string, vehicleID str
 	common.Print("notiToken", notiToken)
 	topicAuction := fmt.Sprintf("JOIN_AUCTION_%s", auctionID)
 	common.Print("topicAuction", topicAuction)
+
 	if err := common.AddUserToSubscription(notiToken, topicAuction); err != nil {
 		return err
 	}
+	topicAuctionVehicle := fmt.Sprintf("JOIN_AUCTION_VEHICLE_%s_%s", auctionID, vehicleID)
+	common.Print("topicAuctionVechile", topicAuctionVehicle)
 
-	topicAuctionVechile := fmt.Sprintf("JOIN_AUCTION_VEHICLE_%s_%s", auctionID, vehicleID)
-	common.Print("topicAuctionVechile", topicAuctionVechile)
-	return common.AddUserToSubscription(notiToken, topicAuctionVechile)
+	// notificationtopics := midOrm.NotificationTopic{
+	// 	ID:   uuid.NewString(),
+	// 	Name: topicAuctionVechile,
+	// }
+
+	// if err := common.Database.Debug().Create(&notificationtopics).Error; err != nil {
+	// 	return fmt.Errorf("failed to insert notificationtopics: %v", err)
+	// }
+	// var existingTopic midOrm.NotificationTopic
+	// err := common.Database.Where("name = ?", topicAuctionVehicle).First(&existingTopic).Error
+	// if err != nil {
+	// 	if errors.Is(err, gorm.ErrRecordNotFound) {
+	// 		notificationTopic := midOrm.NotificationTopic{
+	// 			ID:   uuid.NewString(),
+	// 			Name: topicAuctionVehicle,
+	// 		}
+	// 		if err := common.Database.Debug().Create(&notificationTopic).Error; err != nil {
+	// 			common.PrintError("Failed to insert notification topic: ", err.Error())
+	// 		}
+	// 	} else {
+	// 		common.PrintError("Failed to check existing topic: ", err.Error())
+	// 	}
+	// } else {
+	// 	common.Print("Topic already exists: ", topicAuctionVehicle)
+	// }
+	return common.AddUserToSubscription(notiToken, topicAuctionVehicle)
 }
 
 func NotiRemoveUserAuctionVehicle(notiToken string, auctionID string, vehicleID string) error {
