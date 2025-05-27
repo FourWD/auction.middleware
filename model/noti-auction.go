803800c3923947672fb25ca75bf2d4d3c03bc65d
PAKKAD\puarns <puarns@gmail.com>
2025-05-28 00:40:00 +0700

    no message

--- model/noti-auction.go
+++ model/noti-auction.go
@@ -1,6 +1,7 @@
 package model
 
-type AuctionNoti struct {
+type NotiAuction struct {
 	AuctionID string `json:"auction_id" query:"auction_id"`
 	VehicleID string `json:"vehicle_id" query:"vehicle_id"`
+	UserID    string `json:"user_id" query:"user_id"`
 }
