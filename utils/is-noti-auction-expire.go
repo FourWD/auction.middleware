8e3df0ccac52dbbbd3127e03cd3fe834ee266717
PAKKAD\puarns <puarns@gmail.com>
2025-06-11 19:00:27 +0700

    no message

--- utils/is-noti-auction-expire.go
+++ utils/is-noti-auction-expire.go
@@ -6,7 +6,7 @@ import (
 	"github.com/FourWD/auction.middleware/model"
 )
 
-func IsNotiAuctionExpire(noti model.NotiAuction) bool {
+func IsNotiAuctionExpire(noti *model.NotiAuction) bool {
 	expire := noti.ActionTime.Add(5 * time.Minute)
 	return time.Now().After(expire)
 }
