b72bf2a3690283e3700f7afff9b45f5be8f0b0f4
PAKKAD\puarns <puarns@gmail.com>
2025-06-11 18:40:34 +0700

    no message

--- model/noti-auction.go
+++ model/noti-auction.go
@@ -1,9 +1,12 @@
 package model
 
+import "time"
+
 type NotiAuction struct {
-	AuctionID string `json:"auction_id" query:"auction_id"`
-	VehicleID string `json:"vehicle_id" query:"vehicle_id"`
-	UserID    string `json:"user_id" query:"user_id"`
-	Price     int    `json:"price" query:"price"`
-	NewPrice  int    `json:"new_price" query:"new_price"`
+	AuctionID  string    `json:"auction_id" query:"auction_id"`
+	VehicleID  string    `json:"vehicle_id" query:"vehicle_id"`
+	UserID     string    `json:"user_id" query:"user_id"`
+	Price      int       `json:"price" query:"price"`
+	NewPrice   int       `json:"new_price" query:"new_price"`
+	ActionTime time.Time `json:"action_time" query:"action_time"`
 }
